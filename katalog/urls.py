734aae18bc33d4c5b1e730f4cdf3861c8fe531ca
Ruzain <muhammadruzain10@gmail.com>
2022-09-11 16:23:12 +0700

    Selesai membuat fungsi pada views.py

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from katalog.views import show_katalog
+
 # TODO: Implement Routings Here
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_katalog, name='show_katalog'),
+]
